b08d7110de0c1df3240a6a608bb4bcbd64895722
Divyansh Goyal <divyansh.goyal2001@gmail.com>
2022-10-01 13:20:18 -0600

    Small update

--- ReTain/app/views.py
+++ ReTain/app/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
 
 # Create your views here.
+def index(request):
+    if request.method != "POST":
+        image_url = request.POST["image"]
+    return render(request, "index.html")
+
